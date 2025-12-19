fd5031d0cf1c2305ebaeb3af899eec24a96699c5
Ogulcan Aydogan <ogulcanaydogan@hotmail.com>
2025-12-19 09:11:14 +0000

    refactor: simplify output value expressions in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,14 @@
 output "fqdn" {
   description = "Fully qualified DNS name of the record"
-  value = local.alias_mode
-    ? aws_route53_record.alias_a[0].fqdn
-    : local.simple_mode
-    ? aws_route53_record.simple[0].fqdn
-    : null
+  value       = local.alias_mode ? aws_route53_record.alias_a[0].fqdn : (local.simple_mode ? aws_route53_record.simple[0].fqdn : null)
 }
 
 output "record_name" {
   description = "Record name"
-  value = local.alias_mode
-    ? aws_route53_record.alias_a[0].name
-    : local.simple_mode
-    ? aws_route53_record.simple[0].name
-    : null
+  value       = local.alias_mode ? aws_route53_record.alias_a[0].name : (local.simple_mode ? aws_route53_record.simple[0].name : null)
 }
 
 output "zone_id" {
   description = "Hosted zone ID containing the record"
-  value = local.alias_mode
-    ? aws_route53_record.alias_a[0].zone_id
-    : local.simple_mode
-    ? aws_route53_record.simple[0].zone_id
-    : null
+  value       = local.alias_mode ? aws_route53_record.alias_a[0].zone_id : (local.simple_mode ? aws_route53_record.simple[0].zone_id : null)
 }
